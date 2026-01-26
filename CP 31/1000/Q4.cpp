de5d55024229a4bfd86fe65dfc7f35c85aa3b3af
Pratham Raj <prathamraj2411@gmail.com>
2026-01-26 20:06:24 +0530

    Add files via upload

--- CP 31/1000/Q4.cpp
+++ CP 31/1000/Q4.cpp
@@ -9,28 +9,18 @@ int main() {
     while(t--){
         int n;
         cin>>n;
-        vector<vector<int>> arr(n);
-        int ans=0;
+        long long ans=0; long long second_sum=0; int min_second=1e9,min_first=1e9;
         for(int i=0;i<n;i++){
             int m;
             cin>>m;
             vector<int> temp(m);
             for(int i=0;i<m;i++) cin>>temp[i];
             sort(temp.begin(),temp.end());
-            ans+=temp[0];
-            arr.push_back(temp);
+            second_sum+=temp[1];
+            min_first=min(min_first,temp[0]);
+            min_second=min(min_second,temp[1]);
         }
-       for(auto array1:arr){
-         bool move=0; 
-         int benifit=array1[1]-array1[0];
-         int maxb=benifit;
-         for(auto array2:arr){
-            if(array1!=array2){
-              int loss=array2[0]-min(array1[0],array2[0]);
-              
-            }
-         }
-       }
+      ans=second_sum-min_second+min_first;
        cout<<ans<<"\n";  
     }
     return 0;
