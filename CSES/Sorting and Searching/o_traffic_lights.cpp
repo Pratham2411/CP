5c7456c4d3cb72409b2913c30e767b37962370ff
Pratham Raj <prathamraj2411@gmail.com>
2026-02-14 00:21:29 +0530

    Add files via upload

--- CSES/Sorting and Searching/o_traffic_lights.cpp
+++ CSES/Sorting and Searching/o_traffic_lights.cpp
@@ -1,1 +1,36 @@
 // https://cses.fi/problemset/task/1163
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int x, n;
+  cin >> x >> n;
+
+  set<int> pos = {0, x};
+  multiset<int> len;
+  len.insert(x);
+
+  for(int i = 0; i < n; i++){
+    int p; 
+    cin >> p;
+
+    auto it = pos.upper_bound(p);
+    int right = *it;
+    int left = *prev(it);
+
+    // Using find() ensures we only delete one instance of the length
+    len.erase(len.find(right - left));
+    
+    len.insert(p - left);
+    len.insert(right - p);
+    pos.insert(p);
+    cout << *len.rbegin() << " ";
+  }
+  cout << "\n";
+  
+  return 0;
+}
+
+
